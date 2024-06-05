0daf44b138efdf9c35872928335fa90691fe91c2
Tezz <28359093+Tessie27@users.noreply.github.com>
2024-06-05 20:34:05 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,40 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-*/
-// Scripts
-
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+function showExperience() {
+    toggleDetails('experience');
+  }
+  
+  function showEducation() {
+    toggleDetails('education');
+  }
+  
+  function showCertification() {
+    toggleDetails('certification');
+  }
+  
+  function showSkills() {
+    toggleDetails('skills');
+  }
+  
+  function showHonours() {
+    toggleDetails('honours');
+  }
+  
+  function showBadges() {
+    toggleDetails('badges');
+  }
+  
+  function toggleDetails(id) {
+    var details = document.getElementById(id);
+    var allDetails = document.querySelectorAll('.details');
+  
+    allDetails.forEach((item) => {
+      if (item.id !== id && item.style.display !== 'none') {
+        item.style.display = 'none';
+      }
     });
-
-});
+  
+    if (details.style.display === 'none' || details.style.display === '') {
+      details.style.display = 'block';
+    } else {
+      details.style.display = 'none';
+    }
+  }
